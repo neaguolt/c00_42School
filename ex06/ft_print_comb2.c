484991334f63f6b9f0c164e660274931a1db119b
neaguolt <neaguolt@gmail.com>
2024-07-18 17:22:56 +0300

    Add ex06 ft_print_comb2 with write()

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -6,17 +6,38 @@
 /*   By: deb <neaguolt@gmail.com>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 09:22:56 by deb               #+#    #+#             */
-/*   Updated: 2024/07/18 09:25:42 by deb              ###   ########.fr       */
+/*   Updated: 2024/07/18 17:21:42 by deb              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-//function prototype
+// function prototype
 void	ft_print_comb2(void);
 
-//function declaration
+// function declaration
 void	ft_print_comb2(void)
 {
+	char	digit_no[5];
+	int		no1;
+	int		no2;
 
+	no1 = 0;
+	while (no1 < 98)
+	{
+		no2 = no1 + 1;
+		while (no2 <= 99)
+		{
+			digit_no[0] = '0' + (no1 / 10);
+			digit_no[1] = '0' + (no1 % 10);
+			digit_no[2] = ' ';
+			digit_no[3] = '0' + (no2 / 10);
+			digit_no[4] = '0' + (no2 % 10);
+			write(1, digit_no, 5);
+			write(1, ", ", 2);
+			no2++;
+		}
+		no1++;
+	}
+	write(1, "98 99", 5);
 }
