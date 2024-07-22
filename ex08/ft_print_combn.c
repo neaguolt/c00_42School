98deed6a9be43671610b00046710835c9cec4b88
neaguolt <neaguolt@gmail.com>
2024-07-22 16:31:33 +0300

    Add valid version to ex08 ft_print_combon

--- ex08/ft_print_combn.c
+++ ex08/ft_print_combn.c
@@ -6,55 +6,34 @@
 /*   By: deb <neaguolt@gmail.com>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 09:06:39 by deb               #+#    #+#             */
-/*   Updated: 2024/07/19 18:09:54 by deb              ###   ########.fr       */
+/*   Updated: 2024/07/22 16:06:22 by deb              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-//function prototype
-void	ft_print_combn(int n);
-
-//function declaration
 void	ft_print_combn(int n)
 {
-	char	digit_buffer[10];
+	char	digit_buffer[11];
 	int		i;
-	int		done;
-	int		poz;
 
+	if (n < 1 || n > 9)
+		return ;
+	*digit_buffer = '0';
 	i = 0;
-	while (i < n)
+	while (++i < n)
+		digit_buffer[i] = digit_buffer[i - 1] + 1;
+	digit_buffer[n] = ',';
+	digit_buffer[n + 1] = ' ';
+	while (*digit_buffer < 58 - n)
 	{
-		digit_buffer[i] = '0' + i;
-		i++;
+		write(1, digit_buffer, n + 2);
+		i = n;
+		while (i--)
+			if (++digit_buffer[i] <= 58 - n + i)
+				break ;
+		while (++i > 0 && i < n)
+			digit_buffer[i] = digit_buffer[i - 1] + 1;
 	}
-	done = 0;
-	while (!done)
-	{
-		write(1, digit_buffer, n);
-		if (! poz == 0 || ! (digit_buffer[poz] == ('9' - (n - 1 - poz))))
-		{
-			write(1, ", ", 2);
-		}
-		poz = n - 1;
-		while (poz >= 0 && digit_buffer[poz] == ('9' - (n - 1 - poz)))
-		{
-			poz = poz - 1;
-		}
-		if (poz < 0)
-		{
-			done = 1;
-		}
-		else
-		{
-			digit_buffer[poz] = digit_buffer[poz] + 1;
-			i = poz + 1;
-			while (i < n)
-			{
-				digit_buffer[i] = digit_buffer[i - 1] + 1;
-				i++;
-			}
-		}
-	}
+	write(1, digit_buffer, n);
 }
